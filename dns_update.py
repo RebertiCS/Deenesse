34066e18f1423ba1b7401523f292859008cb5f26
reberti <rebertics+dev@gmail.com>
2025-09-27 11:09:09 -0300

    Fallowing pylint tips

--- dns_update.py
+++ dns_update.py
@@ -16,7 +16,7 @@ CF_URL = "https://api.cloudflare.com/client/v4/zones/"
 def main():
     """DNS updater."""
     print(
-        "# Deenesse v1.0",
+        "# Deenesse v1.1",
         datetime.now().strftime("%d/%m/%Y - %H:%M"),
     )
 
@@ -34,15 +34,14 @@ def main():
 
         else:
             print(f"[ {datetime.now()} ] IPv4: {ipv4}")
+
     except requests.exceptions.Timeout:
         print(f"[ {datetime.now()} ] Connection timeout while getting IPv4")
 
     try:
         dns_list = os.getenv("CF_DNS").split(",")
     except KeyError:
-        print(
-            f"[ {datetime.now()} ] Missing or wrong configuration on .env. Var CF_DNS"
-        )
+        print(f"[ {datetime.now()} ] Wrong configuration CF_DNS")
         return
 
     if ipv4:
@@ -51,48 +50,43 @@ def main():
     req_data = get_config()
 
     if req_data is None:
-        print(f"[ {datetime.now()} ] Failed to retrieve configuration, exiting.")
-        return -1
+        print(f"[ {datetime.now()} ] Failed to retrieve configuration")
+        return
 
     print(f"[ {datetime.now()} ] DNS Updates:")
 
     for dns in req_data["result"]:
         for dns_name in dns_list:
             if dns_name == dns["name"]:
-
-                if dns["type"] == "AAAA":
-                    if ipv6 is None:
-                        print(f"[ {datetime.now()} ] IPv6 missing, won't update")
+                if dns["type"] == "AAAA" and ipv6 is not None:
                     # Update IPv6 if it has changed
                     if dns["content"] != ipv6:
                         update_config(dns["name"], ipv6, dns["id"], dns["type"])
 
                         print(
-                            f"Name:{dns_name} \n Type: {dns['type']} \n - Old: {dns['content']} \n - New: {ipv6}"
+                            f"Name:{dns_name} \n Type: {dns['type']}",
+                            f"\n - Old: {dns['content']} \n - New: {ipv6}",
                         )
 
-                    # Logs when the IPv6 dind't change
                     else:
                         print(
-                            f"Name: {dns_name} \nType: {dns['type']} \n - IP {ipv6} \n - Same IPv6, won't update"
+                            f"Name: {dns_name} \nType: {dns['type']}",
+                            f"\n - IP {ipv6} \n - Same IPv6, won't update",
                         )
-                else:
-                    if ipv4 is None:
-                        print(f"[ {datetime.now()} ] IPv4 missing, won't update")
-                        return
-
+                elif ipv4 is not None:
                     # Update IPv4 if it has changed
                     if dns["content"] != ipv4:
                         update_config(dns["name"], ipv4, dns["id"], dns["type"])
 
                         print(
-                            f"Name: {dns_name} \nType: {dns['type']} \n - Old: {dns['content']} \n - New: {ipv4}"
+                            f"Name: {dns_name} \nType: {dns['type']} ",
+                            f"\n - Old: {dns['content']} \n - New: {ipv4}",
                         )
 
-                    # Logs when the IPv4 dind't change
                     else:
                         print(
-                            f"Name: {dns_name} \nType: {dns['type']} \n - IP {ipv4} \n - Same IPv4, won't update"
+                            f"Name: {dns_name} \nType: {dns['type']} ",
+                            f"\n - IP {ipv4} \n - Same IPv4, won't update",
                         )
 
 
@@ -104,17 +98,13 @@ def get_config():
             "Authorization": "Bearer " + os.getenv("CF_KEY"),
         }
     except KeyError:
-        print(
-            f"[ {datetime.now()} ] Missing or wrong configuration on '.env'. Var CF_KEY"
-        )
+        print(f"[ {datetime.now()} ] Wrong configuration CF_KEY")
         return None
 
     try:
         api_url = CF_URL + os.getenv("CF_ZONE") + "/dns_records"
     except KeyError:
-        print(
-            f"[ {datetime.now()} ] Missing or wrong configuration on '.env'. Var CF_ZONE"
-        )
+        print(f"[ {datetime.now()} ] Wrong configuration CF_ZONE")
         return None
 
     try:
@@ -124,9 +114,7 @@ def get_config():
             headers=headers,
         )
     except requests.exceptions.Timeout:
-        print(
-            f"[ {datetime.now()} ] Connection timeout while getting cloudflare zone configuration"
-        )
+        print(f"[ {datetime.now()} ] Connection timeout while getting zone")
 
         return None
 
@@ -137,7 +125,8 @@ def get_config():
     # Show information about DNS Server
     for data in request_data["result"]:
         print(
-            f" - Name: {data['name']} \n\t+ Id:\t {data['id']} \n\t+ Type:\t {data['type']} \n\t+ IP:\t {data['content']}"
+            f" - Name: {data['name']} \n\t+ Id:\t {data['id']}",
+            f"\n\t+ Type:\t {data['type']} \n\t+ IP:\t {data['content']}",
         )
 
     return request_data
@@ -146,18 +135,10 @@ def get_config():
 def update_config(dns_name, dns_ip, dns_id, dns_type):
     """Update DNS on Cloudflare servers."""
     try:
-        cf_proxy_str = bool(os.getenv("CF_PROXY"))
+        cf_proxy = bool(os.getenv("CF_PROXY"))
     except KeyError:
-        print(
-            f"[ {datetime.now()} ] Missing or wrong configuration on '.env'. Var CF_PROXY"
-        )
-
+        print(f"[ {datetime.now()} ] Wrong configuration CF_PROXY")
         return
-
-    if cf_proxy_str in ["True", "true"]:
-        cf_proxy = True
-    else:
-        cf_proxy = False
 
     try:
         headers = {
@@ -165,9 +146,7 @@ def update_config(dns_name, dns_ip, dns_id, dns_type):
             "Authorization": "Bearer " + os.getenv("CF_KEY"),
         }
     except KeyError:
-        print(
-            f"[ {datetime.now()} ] Missing or wrong configuration on '.env'. Var CF_KEY"
-        )
+        print(f"[ {datetime.now()} ] Wrong configuration CF_KEY")
 
         return
 
@@ -183,9 +162,7 @@ def update_config(dns_name, dns_ip, dns_id, dns_type):
     try:
         api_url = CF_URL + os.getenv("CF_ZONE") + "/dns_records/" + dns_id
     except KeyError:
-        print(
-            f"[ {datetime.now()} ] Missing or wrong configuration on '.env'. Var CF_ZONE"
-        )
+        print(f"[ {datetime.now()} ] Wrong configuration CF_ZONE")
 
         return
 
